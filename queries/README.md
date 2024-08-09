a173d04a44bdac73b45396d4071197a1929e7ed0
karkraz <rochadcmarcos@gmail.com>
2024-08-09 07:09:21 -0300

    add queries main file

---
+++ queries/README.md
@@ -0,0 +1,20 @@
+# Queries
+
+### First Names
+---
+* ID -> Auto 
+* VALUE -> A string with the name value
+* GENDER -> A char correspondent to the value.
+```
+    M -> Male
+    F -> Female
+    N -> Neutral
+```
+* NATIONALITY -> A string with a country name
+
+### Last Names
+---
+* ID -> Auto 
+* VALUE -> A string with the name value
+* NATIONALITY -> A string with a country name
+
---
+++ queries/main.sql
@@ -0,0 +1,45 @@
+USE NameGeneratorDB;
+
+ALTER DATABASE NameGeneratorDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE first_names(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    value VARCHAR(200) NOT NULL,
+    gender CHAR(1) NOT NULL,
+    nationality VARCHAR(100)
+);
+
+CREATE TABLE last_names(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    value VARCHAR(200) NOT NULL,
+    nationality VARCHAR(100)
+);
+
+
+DELIMITER $
+
+CREATE FUNCTION GET_FULL_NAME(FIRST_NAME_NATIONALITY VARCHAR(200), LAST_NAME_NATIONALITY VARCHAR(100))
+RETURNS VARCHAR(400) 
+DETERMINISTIC
+BEGIN
+	DECLARE first_name VARCHAR(200);
+	DECLARE last_name VARCHAR(200);
+
+    SELECT first_names.value        
+    FROM first_names
+    WHERE first_names.nationality = FIRST_NAME_NATIONALITY 
+    ORDER BY RAND() LIMIT 1
+    INTO first_name;
+	
+    SELECT last_names.value
+    FROM last_names
+    WHERE last_names.nationality = LAST_NAME_NATIONALITY
+    ORDER BY RAND() LIMIT 1
+    INTO last_name;
+   	
+    RETURN CONCAT(first_name, ' ', last_name);
+END;
+$
+
+
+DELIMITER ;
