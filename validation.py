ef43c39177169c4907eb80e9285d66c564ac5e94
karkraz <rochadcmarcos@gmail.com>
2024-08-17 19:46:33 -0300

    refactor validation

--- validation.py
+++ validation.py
@@ -2,6 +2,44 @@
 #
 #
 
+def alpha_string(string: str, logical_response: bool) -> bool:
+    """Checks if the string is all alphabetic
+
+    Parameters
+    ----------
+    string : str
+        A string value
+    logical_response : bool
+        Another boolean response for the previous validation
+
+    Returns
+    -------
+    bool : using a 'and' operator between this and the previous validation
+    """
+
+    if ' ' in string:
+        return logical_response and string.replace(' ', '').isalpha()
+    return logical_response and string.isalpha()
+    
+def accurate_len_string(string: str, min_str: int, max_str: int) -> bool:
+    """Check if the strings is between the length intervall
+
+    Parameters
+    ----------
+    string : str
+        String value
+    min_str : int
+        Minimum value for the string
+    max_str : int
+        Maximum value for the string
+    
+    Returns
+    -------
+    bool : based on the string length
+    """
+
+    return min_str <= len(string) <= max_str
+
 def validates_name_nationality(func) -> bool:
     """Validates if the name and nationality strings value is: 
     all alphabetic and his length is between 3 and 200 letters
@@ -17,15 +55,19 @@ def validates_name_nationality(func) -> bool:
     """
     
     def inner1(*args, **kwargs):
-        name, nationality = args[1], args[-1]
-
-        name_val = 3 <= len(name) <= 200 and name.isalpha()
-        nationality_val = 3 <= len(nationality) <= 200 and nationality.isalpha()
+        name, nationality = args[1], args[3]
+    
+        name_val = accurate_len_string(name, 1, 200)
+        nationality_val = accurate_len_string(nationality, 3, 200)
         
+        name_val = alpha_string(name, name_val)
+        nationality_val = alpha_string(nationality, nationality_val)
+
         if name_val and nationality_val:
             return func(*args, **kwargs)
         
-        raise Exception('Name or Nationality string is not set correctly. Please verify')
+        raise Exception(f'Error during name and nationalionality validation.\
+                        Name validation: {name_val} and Nationality validation: {nationality_val}')
 
     return inner1
 
