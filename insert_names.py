557cd9e6fef6aba3fe4c4dc170bbcdbb573cd971
karkraz <rochadcmarcos@gmail.com>
2024-08-22 07:10:12 -0300

    insert first and last names module

--- insert_names.py
+++ insert_names.py
@@ -3,22 +3,34 @@ from alive_progress import alive_bar
 
 controller = NamesController()
 
-failed = 0
+def insert_names(file_path: str, name_pointer: str) -> int:
+    """
+    """
 
-with open('files/first_names.csv', 'r') as file:
-    file = file.readlines()
+    failed = 0 
 
+    with open(file_path, 'r') as file:
+        file = file.readlines()
+        
+        with alive_bar(len(file)) as bar:
+            for f in file:
+                f = f.split(',')
+                nationality = f.pop().strip('\n')
+
+                try:
+                    if name_pointer == 'first_names':
+                        name, gender = f
+                        controller.insert_first_name(name, gender, nationality)
+                    else:
+                        name = f[0]
+                        controller.insert_last_name(name, nationality)
+                except:
+                    failed += 1
+                bar()
+
+    return failed
+
+if __name__ == "__main__":
     print('INSERTING FIRST NAMES INTO DATABASE')
-    with alive_bar(len(file)) as bar:
-        for f in file:
-            f = f.split(',')
-            nationality = f.pop().strip('\n')
-            name, gender = f
-
-            try:
-                controller.insert_first_name(name, gender, nationality)
-            except:
-                failed += 1
-            bar()
-
-print(failed)
+    a = insert_names('files/last_names.csv', 'last_names')
+    print(a)
