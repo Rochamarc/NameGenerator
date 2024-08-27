790b2eb79e06f408ed0da913101e8696367ba135
karkraz <rochadcmarcos@gmail.com>
2024-08-26 21:47:02 -0300

    update docstings

--- base_controller.py
+++ base_controller.py
@@ -8,6 +8,16 @@ CURRENT_PATH = pathlib.Path(__file__).parent.resolve()
 class BaseController:
     """
     Base class that contains every property and functions used in other controllers
+
+    Class Variables
+    ---------------
+    database_config
+        dict containing database configuration 
+
+    Methods
+    -------
+    get_query(db_method, file_name)
+        return a str with a sql query
     """
 
     with open(os.path.join(CURRENT_PATH, 'config.json')) as f:
@@ -23,15 +33,14 @@ class BaseController:
             Name of the action that the user wanna made ex: select | insert
         file_name : str
             Name of the file inside the path without the file extension
+
+        Raises
+        ------
+        FileNotFoundError : If the file doesnt exists        
         
         Returns
         -------
         str : A string with sql query
-
-        Raises
-        ------
-            FileNotFoundError
-                If the file doesnt exists
         """
 
         file_path = os.path.join(
